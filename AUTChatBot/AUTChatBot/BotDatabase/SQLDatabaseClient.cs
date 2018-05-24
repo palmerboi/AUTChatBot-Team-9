42ec94534015226eeba25e90deb9b8922156ddf5
That-Guy-Rudster <16830450+That-Guy-Rudster@users.noreply.github.com>
2018-05-24 23:22:45 +1200

    SQLDatabase

---
+++ AUTChatBot/AUTChatBot/BotDatabase/SQLDatabaseClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace AUTChatBot.BotDatabase
+{
+    public class SQLDatabaseClient
+    {
+
+        public SqlConnection GetSQLConnection()
+        {
+            SqlConnectionStringBuilder cb = new SqlConnectionStringBuilder();
+            cb.DataSource = "sqlserverautpapers.database.windows.net";
+        }
+
+        
+    }
+}
